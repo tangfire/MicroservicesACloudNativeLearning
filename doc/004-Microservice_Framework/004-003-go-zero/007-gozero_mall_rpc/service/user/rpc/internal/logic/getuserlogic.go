909a306228303f09f81d8e8f45280c1f0f07f0fc
tangfire <52628216@qq.com>
2025-07-29 08:41:54 +0800

    etcd no start

--- doc/004-Microservice_Framework/004-003-go-zero/007-gozero_mall_rpc/service/user/rpc/internal/logic/getuserlogic.go
+++ doc/004-Microservice_Framework/004-003-go-zero/007-gozero_mall_rpc/service/user/rpc/internal/logic/getuserlogic.go
@@ -2,6 +2,9 @@ package logic
 
 import (
 	"context"
+	"errors"
+
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
 
 	"MicroservicesACloudNativeLearning/doc/004-Microservice_Framework/004-003-go-zero/007-gozero_mall_rpc/service/user/rpc/internal/svc"
 	"MicroservicesACloudNativeLearning/doc/004-Microservice_Framework/004-003-go-zero/007-gozero_mall_rpc/service/user/rpc/types/user"
@@ -26,5 +29,21 @@ func NewGetUserLogic(ctx context.Context, svcCtx *svc.ServiceContext) *GetUserLo
 func (l *GetUserLogic) GetUser(in *user.GetUserReq) (*user.GetUserResp, error) {
 	// todo: add your logic here and delete this line
 
-	return &user.GetUserResp{}, nil
+	// 根据userID查询数据库返回用户信息
+	one, err := l.svcCtx.UserModel.FindOneByUserId(l.ctx, in.UserID)
+	// 1. 查询数据库失败
+	// 2. 根据userID查不到用户
+	if errors.Is(err, sqlx.ErrNotFound) {
+		return nil, errors.New("user not found")
+	}
+	if err != nil {
+		logx.Errorw("user.rpc.GetUser FindOneByUserId failed", logx.Field("err", err))
+		return nil, errors.New("查询失败")
+	}
+
+	return &user.GetUserResp{
+		UserID:   one.UserId,
+		Username: one.Username,
+		Gender:   one.Gender,
+	}, nil
 }
--- doc/004-Microservice_Framework/004-003-go-zero/007-gozero_mall_rpc/service/user/rpc/internal/svc/servicecontext.go
+++ doc/004-Microservice_Framework/004-003-go-zero/007-gozero_mall_rpc/service/user/rpc/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "MicroservicesACloudNativeLearning/doc/004-Microservice_Framework/004-003-go-zero/007-gozero_mall_rpc/service/user/rpc/internal/config"
+import (
+	"MicroservicesACloudNativeLearning/doc/004-Microservice_Framework/004-003-go-zero/003-gozero_mall_cache/service/user/model"
+	"MicroservicesACloudNativeLearning/doc/004-Microservice_Framework/004-003-go-zero/007-gozero_mall_rpc/service/user/rpc/internal/config"
+	"github.com/zeromicro/go-zero/core/stores/sqlx"
+)
 
 type ServiceContext struct {
-	Config config.Config
+	Config    config.Config
+	UserModel model.UserModel
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
+	conn := sqlx.NewMysql(c.Mysql.DataSource)
 	return &ServiceContext{
-		Config: c,
+		Config:    c,
+		UserModel: model.NewUserModel(conn, c.CacheRedis),
 	}
 }
